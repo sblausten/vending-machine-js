69f8e10d215a386e8c7abd8e8e216887a11198d0
Samuel Blausten <samuel.blausten@bbc.co.uk>
2019-08-22 16:59:16 +0100

    Refactor and additional test coverage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 
-function VendingMachine(products, change) {
+function VendingMachine(products = [], change = {}) {
 
   this.products = products;
   this.change = change;
 
   VendingMachine.prototype.getProducts = function() {
     return this.products;
-  }
+  };
 
   VendingMachine.prototype.getChange = function() {
     return this.change;
-  }
+  };
 
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -7,7 +7,7 @@ describe('VendingMachine', () => {
     const products = [
       { id: "crisps", price: 0.5, quantity: 3 },
       { id: "chocolate", price: 1, quantity: 5 }
-    ]
+    ];
     const vendingMachine = new VendingMachine(products);
 
     expect(vendingMachine.getProducts()).to.equal(products);
@@ -23,9 +23,24 @@ describe('VendingMachine', () => {
       0.50: 60,
       1: 10,
       2: 5,
-    }
+    };
     const vendingMachine = new VendingMachine(null, change);
 
     expect(vendingMachine.getChange()).to.equal(change);
+  });
+
+  it('should save both change and products on initialization', () => {
+    const products = [
+      { id: "crisps", price: 0.5, quantity: 3 },
+      { id: "chocolate", price: 1, quantity: 5 }
+    ];
+    const change = {
+      0.50: 60,
+      1: 10
+    };
+    const vendingMachine = new VendingMachine(products, change);
+
+    expect(vendingMachine.getChange()).to.equal(change);
+    expect(vendingMachine.getProducts()).to.equal(products);
   });
 });
